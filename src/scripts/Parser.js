3e2904674700a8f229cdd7b8303bb29fbe1965e0
Joshua usi <ExperimentatorsY@gmail.com>
2021-01-16 10:29:51 +1100

    Updated Parser to work with timing points

--- src/scripts/Parser.js
+++ src/scripts/Parser.js
@@ -9,24 +9,32 @@ define(function(require) {
 				version: splited[0],
 				hitObjects: [],
 				hitObjectsParsed: [],
+				timingPoints: [],
+				timingPointsParsed: [],
 			}
-			let passedHitObjects = false;
+			let section = "";
 			/* start from 1 to ignore version */
 			for (var i = 1; i < splited.length; i++) {
 				if (splited[i] === "" || splited[i].substr(0, 2) === "//") {
 					continue;
 				}
-				if (splited[i] === "[HitObjects]") {
-					passedHitObjects = true;
+				if (splited[i][0] === "[") {
+					section = splited[i];
 				}
 				let l = splited[i].split(/:(.+)/);
-				if (l.length === 1) {
+				if (section === "[TimingPoints]" && /[,]/g.test(splited[i])) {
+					beatmap.timingPoints.push(splited[i]);
+					beatmap.timingPointsParsed.push(this.parseTimingPoint(splited[i]));
 					continue;
 				}
-				if (passedHitObjects && /\d/.test(l[0])) {
+				if (section === "[HitObjects]" && /[,]/g.test(splited[i])) {
 					beatmap.hitObjects.push(splited[i]);
 					beatmap.hitObjectsParsed.push(this.parseHitObject(splited[i]));
 					beatmap.hitObjectsParsed[beatmap.hitObjectsParsed.length - 1].time -= Formulas.AR(beatmap.ApproachRate);
+					continue;
+				}
+				if (l.length === 1) {
+					console.log(section + ": " + l);
 					continue;
 				}
 				if (isNaN(parseFloat(l[1]))) {
@@ -36,13 +44,6 @@ define(function(require) {
 					beatmap[l[0]] = l[1];
 				} else {
 					beatmap[l[0]] = parseFloat(l[1]);
-				}
-			}
-			for (var i = 0; i < beatmap.hitObjectsParsed.length; i++) {
-				if (typeof(beatmap.hitObjectsParsed[i].x) === "string") {
-					beatmap.hitObjectsParsed.splice(i, 1);
-					beatmap.hitObjects.splice(i, 1);
-					i--;
 				}
 			}
 			return beatmap;
@@ -64,10 +65,17 @@ define(function(require) {
 			} else if (asBinary[3] === "1") {
 				/* spinner */
 				return new HitObject.Spinner(...splited);
+			} else {
 			}
 		},
 		parseTimingPoint: function(data) {
-			
+			let splited = data.split(",");
+			for (var i = 0; i < splited.length; i++) {
+				if (/[0-9.]/g.test(splited[i])) {
+					splited[i] = parseFloat(splited[i]);
+				}
+			}
+			return new HitObject.TimingPoint(...splited);
 		}
 	};
 });
