e5286ce0804e4f02987fb63c15b0d4b8165d3e9e
Joshua usi <ExperimentatorsY@gmail.com>
2021-02-02 10:28:27 +1100

    Added test data
    added unit testing for AR and CS

---
+++ tests/testTables.js
@@ -0,0 +1,54 @@
+define(function(require) {
+	var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+	var ARGUMENT_NAMES = /([^\s,]+)/g;
+	function getParamNames(func) {
+		var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+		var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+		if ( result === null)
+				result = [];
+		return result;
+	}
+
+
+	return {
+		table: function(callback, callbackArguments, expected) {
+			if (typeof(callback) !== "function") {
+				return new TypeError("callback needs to be a function");
+			}
+			if (callbackArguments.length != expected.length) {
+				return new Error("argument length and expected values length are not the same");
+			}
+			for (var i = 0; i < callbackArguments.length; i++) {
+				if (typeof callbackArguments[i] === "object") {
+					let evaluation = (callback(...callbackArguments[i]) === expected[i]);
+					let diff = callback(...callbackArguments[i]) - expected[i];
+					if (Math.abs(diff) <= Number.EPSILON) {
+						evaluation = true;
+					}
+					let tableRow = `<tr>
+						<th>${callback.name}</th>
+						<th>${callbackArguments[i]}</th>
+						<th>${expected[i]}</th>
+						<th>${callback(...callbackArguments[i])}</th>
+						<th style=\"background-color: ${(evaluation) ? "#0f0" : "#f00"};\">Passed: ${evaluation}</th>
+					</tr>`;
+					document.querySelector("table").innerHTML += tableRow;
+				} else {
+					let evaluation = (callback(callbackArguments[i]) === expected[i]);
+					let diff = callback(callbackArguments[i]) - expected[i];
+					if (Math.abs(diff) <= Number.EPSILON) {
+						evaluation = true;
+					}
+					let tableRow = `<tr>
+						<th>${callback.name}</th>
+						<th>${callbackArguments[i]}</th>
+						<th>${expected[i]}</th>
+						<th>${callback(callbackArguments[i])}</th>
+						<th style=\"background-color: ${(evaluation) ? "#0f0" : "#f00"};\">Passed: ${evaluation}</th>
+					</tr>`;
+					document.querySelector("table").innerHTML += tableRow;
+				}
+			}
+		}
+	}
+});
---
+++ tests/utils.AR.test.js
@@ -0,0 +1,29 @@
+define(function(require) {
+	let Test = require("testTables.js");
+	Formulas = require("../src/scripts/Formulas.js");
+	Test.table(Formulas.AR, [
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+	], [
+		1.8,
+		1.68,
+		1.56,
+		1.44,
+		1.32,
+		1.2,
+		1.05,
+		0.9,
+		0.75,
+		0.6,
+		0.45,
+	]);
+});
