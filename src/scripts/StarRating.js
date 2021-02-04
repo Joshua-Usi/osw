76b67cac44a0947d43e35c4c82fa6e9ec18c6a5a
Joshua usi <ExperimentatorsY@gmail.com>
2021-02-04 16:18:28 +1100

    tweaked star rating to be affected by mods
    star rating v1
    
    dt increases individual star rating by 1.5 times
    
    ht increases individual star rating by 0.75 times
    
    hr and hd increase star rating by 0.06x
    
    fl increases star rating by 0.19x

--- src/scripts/StarRating.js
+++ src/scripts/StarRating.js
@@ -1,6 +1,6 @@
 define(function(require) {
 	let utils = require("./utils.js");
-	function difficulty(hitObject1, hitObject2) {
+	function difficulty(hitObject1, hitObject2, mods) {
 		let d = 0;
 		if (hitObject1.type[1] === "1") {
 			if (hitObject1.type[1].slides % 2 === 1) {
@@ -12,31 +12,48 @@ define(function(require) {
 			d = utils.dist(hitObject1.x, hitObject1.y, hitObject2.x, hitObject2.y);
 		}
 		let t = (hitObject2.time - hitObject1.time);
+		if (mods.doubleTime) {
+			t /= 1.5;
+		}
+		if (mods.halfTime) {
+			t /= 0.75;
+		}
 		if (t === 0) {
 			t = 1 / 60;
 		}
 		return ((d + Math.sqrt(t)) / 100) * (1 / (t ** (3 / 4)));
 	}
 
-	return function(beatMap, mods) {
-		let highest = 0;
-		let highestIndex = 0;
-		let starRatingSum = 0;
-		let total = 0;
-		for (let i = 0; i < beatMap.hitObjects.length - 1; i++) {
-			if (beatMap.hitObjects[i].type[3] === "1" || beatMap.hitObjects[i + 1].type[3] === "1") {
-				continue;
+	return {
+		version: 1,
+		calculate: function(beatMap, mods) {
+			let highest = 0;
+			let highestIndex = 0;
+			let starRatingSum = 0;
+			let total = 0;
+			for (let i = 0; i < beatMap.hitObjects.length - 1; i++) {
+				if (beatMap.hitObjects[i].type[3] === "1" || beatMap.hitObjects[i + 1].type[3] === "1") {
+					continue;
+				}
+				let diff = difficulty(beatMap.hitObjects[i], beatMap.hitObjects[i + 1], mods);
+				if (diff > highest) {
+					highest = diff;
+					highestIndex = i;
+				}
+				starRatingSum += diff;
+				total++;
 			}
-			let diff = difficulty(beatMap.hitObjects[i], beatMap.hitObjects[i + 1]);
-			if (diff > highest) {
-				highest = diff;
-				highestIndex = i;
+			let modMultiplier = 1;
+			if (mods.hardRock) {
+				modMultiplier += 0.06;
 			}
-			starRatingSum += diff;
-			total++;
-		}
-		console.log("star rating: " + ((starRatingSum / total / 2 + highest / 1.5) / 2));
-		console.log("highest star rating: " + highest / 1.5 + " at " + beatMap.hitObjects[highestIndex].time);
-		return (starRatingSum / total / 2 + highest / 1.5) / 2;
-	}	
+			if (mods.hidden) {
+				modMultiplier += 0.06;
+			}
+			if (mods.flashLight) {
+				modMultiplier += 0.19;
+			}
+			return (starRatingSum * modMultiplier / total / 2 + highest / 1.5) / 2;
+		},
+	}
 });
